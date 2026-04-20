99594cbf91dbc45e494c0f5866a9cf90bbf78ff4
Febecos - Bombeo Solar <febecos.ar@gmail.com>
2026-04-19 23:23:24 -0300

    Update geocode.js

--- pages/api/geocode.js
+++ pages/api/geocode.js
@@ -1,1 +1,17 @@
-
+export default async function handler(req, res) {
+  const { localidad, provincia } = req.query
+  const apiKey = process.env.MAPS_KEY // sin NEXT_PUBLIC_
+  
+  const address = encodeURIComponent(`${localidad}, ${provincia}, Argentina`)
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
+  )
+  const data = await response.json()
+  
+  if (data.results?.[0]) {
+    const loc = data.results[0].geometry.location
+    res.json({ lat: loc.lat, lng: loc.lng })
+  } else {
+    res.json({ lat: null, lng: null })
+  }
+}
