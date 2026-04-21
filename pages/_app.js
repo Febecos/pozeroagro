cd70611c34c1436d87266af58a9745fd641b6433
Febecos - Bombeo Solar <febecos.ar@gmail.com>
2026-04-21 15:06:59 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,50 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+
+const GA_ID = 'G-35TZLZ0ZPW'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  // Trackear cambios de página (navegaciones internas en SPA)
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === 'function') {
+        window.gtag('config', GA_ID, {
+          page_path: url,
+        })
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Google Analytics 4 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="ga-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+              anonymize_ip: true
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
