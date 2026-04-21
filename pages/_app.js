b5850969bae44d655b18381c4f9ccfac8849cafb
Febecos - Bombeo Solar <febecos.ar@gmail.com>
2026-04-21 18:45:36 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import Script from 'next/script'
 
 const GA_ID = 'G-35TZLZ0ZPW'
+const FB_PIXEL_ID = '1495540711927574'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
@@ -10,10 +11,15 @@ export default function App({ Component, pageProps }) {
   // Trackear cambios de página (navegaciones internas en SPA)
   useEffect(() => {
     const handleRouteChange = (url) => {
+      // Google Analytics
       if (typeof window.gtag === 'function') {
         window.gtag('config', GA_ID, {
           page_path: url,
         })
+      }
+      // Meta Pixel
+      if (typeof window.fbq === 'function') {
+        window.fbq('track', 'PageView')
       }
     }
     router.events.on('routeChangeComplete', handleRouteChange)
@@ -44,6 +50,37 @@ export default function App({ Component, pageProps }) {
           `,
         }}
       />
+
+      {/* Meta Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      {/* Meta Pixel fallback (noscript para navegadores sin JS) */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+
       <Component {...pageProps} />
     </>
   )
